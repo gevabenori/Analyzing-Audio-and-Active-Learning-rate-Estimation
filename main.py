1d1fca29c8141dedb884053c5b5f1014f2734e8c
gevab <gevab@campus.technion.ac.il>
2022-01-16 21:12:03 +0200

    Initial commit!!!

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils import convert_video_to_audio_moviepy
+
+import os
+print(os.getcwd())
+
+INPUT_VIDEO_PATH = 'Data/Dennis Lloyd - Anxious (Live at Golan Heights).mp4'
+
+#convert_video_to_audio_moviepy(INPUT_VIDEO_PATH)
+
+
+
